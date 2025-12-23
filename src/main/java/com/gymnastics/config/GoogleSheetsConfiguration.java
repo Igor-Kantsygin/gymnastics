ffe28bc5f48b2629423b95552667c230924cf583
grinn <grinnosse@gmail.com>
2025-12-23 20:03:45 +0300

    Initial

--- src/main/java/com/gymnastics/config/GoogleSheetsConfiguration.java
+++ src/main/java/com/gymnastics/config/GoogleSheetsConfiguration.java
@@ -7,13 +7,11 @@ import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.GoogleCredentials;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
 import java.util.List;
 
@@ -23,17 +21,26 @@ public class GoogleSheetsConfiguration {
     private static final String APPLICATION_NAME = "Telegram bot";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
     private static final List<String> SCOPES = List.of(SheetsScopes.SPREADSHEETS);
-    @Value("${google.sheets.credentials-file}")
-    private String credentialsFile;
 
     @Bean
-    public Sheets sheetsService() throws IOException, GeneralSecurityException {
-        InputStream in = new ClassPathResource(credentialsFile).getInputStream();
-        GoogleCredentials credentials = GoogleCredentials.fromStream(in).createScoped(SCOPES);
+    public Sheets sheetsService() throws Exception {
+
+        String json = System.getenv("GOOGLE_CREDENTIALS_JSON");
+
+        if (json == null || json.isBlank()) {
+            throw new IllegalStateException("GOOGLE_CREDENTIALS_JSON env variable is missing");
+        }
+
+        GoogleCredentials credentials = GoogleCredentials
+                .fromStream(new ByteArrayInputStream(json.getBytes(StandardCharsets.UTF_8)))
+                .createScoped(SCOPES);
+
         return new Sheets.Builder(
                 GoogleNetHttpTransport.newTrustedTransport(),
                 JSON_FACTORY,
                 new HttpCredentialsAdapter(credentials)
-        ).setApplicationName(APPLICATION_NAME).build();
+        )
+                .setApplicationName(APPLICATION_NAME)
+                .build();
     }
 }
